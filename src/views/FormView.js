1ea1a03e7f991c20266625d31ab6c59969ee6c8b
Frida Waldt <fridawaldt@gmail.com>
2022-08-17 11:17:15 +0200

    testing to see if this still works

--- src/views/FormView.js
+++ src/views/FormView.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export const FormView = () => {
-  const [title, setTitle] = useState("");
-  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
   const [rating, setRating] = useState(0);
 
   const handleSubmit = (e) => {
@@ -16,28 +16,28 @@ export const FormView = () => {
     <>
       <h1>Add a movie</h1>
       <form>
-        <div className="title-group">
+        <div className='title-group'>
           <h3>Title</h3>
           <input
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           ></input>
         </div>
-        <div className="url-group">
+        <div className='url-group'>
           <h3>Url</h3>
           <input value={url} onChange={(e) => setUrl(e.target.value)}></input>
         </div>
-        <div className="rating-group">
-          <h3>Rating</h3>
+        <div className='rating-group'>
+          <h3>Ratings</h3>
           <select value={rating} onChange={handleChange}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
+            <option value='1'>1</option>
+            <option value='2'>2</option>
+            <option value='3'>3</option>
+            <option value='4'>4</option>
+            <option value='5'>5</option>
           </select>
         </div>
-        <button type="submit" onClick={handleSubmit}>
+        <button type='submit' onClick={handleSubmit}>
           Submit
         </button>
       </form>
