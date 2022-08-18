576dac211f9ba76fd70d0e51fffbe777484044a9
Frida Waldt <frida.waldt@geshdo.com>
2022-08-18 09:11:11 +0200

    working on design of the app

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,59 +1,84 @@
-import React, { useState } from 'react';
-import { addDoc, collection } from 'firebase/firestore';
-import { db } from '../../lib/init-firebase';
-import './Form.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../lib/init-firebase";
+import "./Form.css";
 
 export const Form = () => {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
+  let navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
   const [rating, setRating] = useState(0);
   const [submit, setSubmit] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (title === '' || url === '' || rating === 0) {
+    if (title === "" || url === "" || rating === 0) {
       return;
     }
-    const moviesCollection = collection(db, 'movies');
+    const moviesCollection = collection(db, "movies");
     addDoc(moviesCollection, { title: title, url: url, rating: rating });
     setSubmit(true);
   };
 
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate("/movies");
+    setTitle("");
+    setUrl("");
+    setRating(0);
+    setSubmit(false);
+  };
+
   return (
-    <div className='form-group'>
-      <h1>Add a movie</h1>
-      <form onSubmit={handleSubmit} className='form'>
-        <div className='title-group'>
-          <h3>Title</h3>
+    <div className="form-group">
+      <h1 className="form-title">Add a movie</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <div className="title-group">
+          <h3 className="form-labels">Title</h3>
           <input
             value={title}
             onChange={(e) => setTitle(e.target.value)}
+            className="form-elements"
           ></input>
         </div>
-        <div className='url-group'>
-          <h3>Url</h3>
-          <input value={url} onChange={(e) => setUrl(e.target.value)}></input>
+        <div className="url-group">
+          <h3 className="form-labels">Url</h3>
+          <input
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+            className="form-elements"
+          ></input>
         </div>
-        <div className='rating-group'>
-          <h3>Ratings</h3>
-          <select value={rating} onChange={(e) => setRating(e.target.value)}>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
+        <div className="rating-group">
+          <h3 className="form-labels">Ratings</h3>
+          <select
+            className="form-elements"
+            value={rating}
+            onChange={(e) => setRating(e.target.value)}
+          >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
           </select>
         </div>
-        <button type='submit' className='submit-btn'>
+        <button type="submit" className="form-elements submit-btn">
           Submit
         </button>
         {submit ? (
-          <div className='submitted-msg'>
-            <h3>You submitted a movie, check it out!</h3>
-            <button>Click here</button>
+          <div className="submitted-msg">
+            <h3 className="submitted-text">
+              You submitted a movie, check it out!
+            </h3>
+            <button className="submitted-btn" onClick={handleClick}>
+              Click here
+            </button>
           </div>
         ) : (
-          ''
+          ""
         )}
       </form>
     </div>
--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { collection, doc, getDocs } from 'firebase/firestore';
-import { db } from '../../lib/init-firebase';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { collection, doc, getDocs } from "firebase/firestore";
+import { db } from "../../lib/init-firebase";
+import "./Movies.css";
 
 export const Movies = () => {
+  let navigate = useNavigate();
+
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -10,7 +14,7 @@ export const Movies = () => {
   }, []);
 
   const getMovies = () => {
-    const movieCollection = collection(db, 'movies');
+    const movieCollection = collection(db, "movies");
     getDocs(movieCollection).then((response) => {
       console.log(response.docs);
       const moviesDocs = response.docs.map((movie) => ({
@@ -22,18 +26,25 @@ export const Movies = () => {
   };
 
   return (
-    <section className='movies-section'>
-      <h1>Movies to watch!</h1>
-      <div className='movies-group'>
+    <section className="movies-section">
+      <h1 className="movies-title">Movies to watch!</h1>
+      <div className="movies-group">
         {movies.map((movie) => (
-          <section>
-            <h3>{movie.data.title}</h3>
-            <a href={movie.data.url}>IMDB</a>
-            <h3>{movie.data.rating}/5</h3>
+          <section className="movies-item-group">
+            <h3 className="movies-item-title ">{movie.data.title}</h3>
+            <a className="movies-item-url " href={movie.data.url}>
+              IMDB
+            </a>
+            <h3 className=" movies-item-rating ">{movie.data.rating}/5</h3>
           </section>
         ))}
       </div>
-      <button>Submit more movies</button>
+      <div className="submit-more-group">
+        <h4 className="submit-more-title">Go submit more movies!</h4>
+        <button className="submit-more-btn" onClick={(e) => navigate("/")}>
+          Click here
+        </button>
+      </div>
     </section>
   );
 };
